dd3ae1e00d8ee71d6f648c0b0ccd219288fcb469
Regiane <regianemelo1995@gmail.com>
2024-07-19 10:46:21 -0300

    rodando testes

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -1,9 +1,14 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
-client = TestClient(app)
-
 
 def test_read_root_deve_retornar_ok_e_ola_mundo():
-    ...
+    client = TestClient(app)  # Arrange (organização do teste)
+
+    response = client.get('/')  # Act (Ação)
+
+    assert response.status_code == HTTPStatus.OK  # afirmação
+    assert response.json() == {'mensagem': 'Olá, mundo!'}
