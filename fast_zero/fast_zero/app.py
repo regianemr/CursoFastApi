f98fc7ed952c8c16e573a3267b55100f969a37bc
Regiane <regianemelo1995@gmail.com>
2024-07-22 17:38:57 -0300

    criando um bc falso

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,12 +1,34 @@
 from fastapi import FastAPI
 
+# from fastapi.responses import HTMLResponse
+
 from http import HTTPStatus
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'mensagem': 'Olá, mundo!'}
+
+
+@app.post('/users/', response_model=UserPublic)
+def create_user(user: UserSchema):
+
+    return user
+
+# @app.get('/', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+# def read_root():
+#     return """
+#     <html>
+#         <head>
+#             <title> Nosso olá mundo!</title>
+#         </head>
+#         <body>
+#             <h1> Olá Mundo </h1>
+#         </body>
+#     </html> """
--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -12,3 +12,12 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # afirmação
     assert response.json() == {'mensagem': 'Olá, mundo!'}
+
+
+def test_html_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange (organização do teste)
+
+    response = client.get('/')  # Act (Ação)
+
+    assert response.status_code == HTTPStatus.OK  # afirmação
+    assert response.json() == {'mensagem': 'Olá, mundo!'}
