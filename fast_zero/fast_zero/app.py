d9a2c88f1e0010ed00f367844b6c0a08015f1496
Regiane <regianemelo1995@gmail.com>
2024-07-23 13:10:18 -0300

    importando http exception

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
@@ -26,3 +26,18 @@ def create_user(user: UserSchema):
 @app.get('/users/', response_model=UserList)
 def read_users():
     return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
